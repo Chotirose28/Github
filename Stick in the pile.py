7bd7fa4b15098e552defa712b40a78348d7bfc78
Farishine28 <chotirose.pra@gmail.com>
2021-07-26 23:03:03 +0700

    26072021_3

--- Stick in the pile.py
+++ Stick in the pile.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sun Jul 25 21:34:34 2021
-Stick in the pile
+HW A3 Stick in the pile
 @author: Chotirose
 """
 
-number_stick = int(input('How many stick (N) in the pile?: '))
+number_stick = int(input('How many sticks (N) in the pile?: '))
 print("There are",number_stick,"sticks in the pile.")
 name = str(input("What is your name: "))
 
@@ -14,7 +14,7 @@ def player(number):
     if number_take > 0:
         if number_take <= 2:
             if number_take <= number_stick:
-                print(name + ', take', number_take, 'stick')
+                pass
             else:
                 number_take = 0
                 print('There are not enough stick to take')
@@ -31,32 +31,38 @@ def com(number):
         return 2
     else:
         return 1
-
+    
+def print_result(number):
+    if number == 1:
+        print('There is', number_stick, 'in the pile')
+    if number > 1:
+        print('There are', number_stick, 'in the pile')
 
 while number_stick > 0:
-    number_take = player(number_stick)
-    if number_take != 0:
-        print(number_take)
-        number_stick = number_stick - number_take
-        print(number_stick)
-    else:
-        continue
-    if number_stick == 0:
-        print(name + ', take the last stick.')
-        print('I, smart computer, win!!!!')
-        break
-    
     number_com_take = com(number_stick)
-    print('I, smart computer, take', number_com_take)
     number_stick = number_stick - number_com_take
+    print('I, smart computer, takes', number_com_take)
+    print_result(number_stick)
     if number_stick == 0:
         print('I, smart computer take the last stick.')
         print(name, 'win. I, smart computer, am sad T_T')
     
-    
+    m = True
+    while m == True and number_stick > 0:
+        number_take = player(number_stick)
+        if number_take != 0:
+            number_stick = number_stick - number_take
+            print_result(number_stick)
+            m = False
+        else:
+            continue
+        if number_stick == 0:
+            print(name + ', takes the last stick.')
+            print('I, smart computer, win!!!!')
+
 
         
-        
+     
         
         
         
