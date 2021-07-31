aa6639e816cdd585a55842261712edc674b97b2a
Farishine28 <chotirose.pra@gmail.com>
2021-07-31 16:34:51 +0700

    31072021_2

--- HW04_B1.py
+++ HW04_B1.py
@@ -5,24 +5,40 @@ HW04_B1
 @author: Chotirose
 """
 l = []
+l2 = []
 m = True
-a = '0'
 
 def recieve(m):
     a = int(input('Type your number: '))
     if a >= 0:
         l.append(str(a))
+    return l
 
 while m == True:
     recieve(m)
     if len(l) == 4:
         m = False
 
-for i in range (len(l)):
-    for j in range (i+1, len(l)):
-        if l[i] + l[j] > l[j] + l[i]:
-            if l[i]+a > a+l[i]:
-                a = l[i]
-        else:
-            if l[j]+a > a + l[j]:
-                a = l[j]
+n = True
+while len(l) > 0:
+    b = '0'
+    for i in range (len(l)):
+        for j in range (i+1, len(l)):
+            if l[i] + l[j] > l[j] + l[i]:
+                if l[i]+b > b+l[i]:
+                    b = l[i]
+            else:
+                if l[j]+b > b + l[j]:
+                    b = l[j]
+    l2.append(b)
+    l.remove(b)
+    if len(l) == 1:
+        l2.append(l[0])
+        l.remove(l[0])
+
+biggest = ""
+for i in range (len(l2)):
+    biggest += l2[i]
+
+print(biggest)
+
