c38f709c9242d893f967cc50fac2e548afc5bf8d
Farishine28 <chotirose.pra@gmail.com>
2021-07-31 15:45:58 +0700

    31072021

--- HW04_B1.py
+++ HW04_B1.py
@@ -6,13 +6,23 @@ HW04_B1
 """
 l = []
 m = True
+a = '0'
 
-while m == True:
+def recieve(m):
     a = int(input('Type your number: '))
     if a >= 0:
-        l.append(a)
-    else:
-        continue
+        l.append(str(a))
+
+while m == True:
+    recieve(m)
     if len(l) == 4:
         m = False
 
+for i in range (len(l)):
+    for j in range (i+1, len(l)):
+        if l[i] + l[j] > l[j] + l[i]:
+            if l[i]+a > a+l[i]:
+                a = l[i]
+        else:
+            if l[j]+a > a + l[j]:
+                a = l[j]
