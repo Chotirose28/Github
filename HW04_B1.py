fdf494faca9a8491a398e73c6d867d08838dafaf
Farishine28 <chotirose.pra@gmail.com>
2021-08-01 23:30:38 +0700

    01082021

--- HW04_B1.py
+++ HW04_B1.py
@@ -6,20 +6,36 @@ HW04_B1
 """
 l = []
 l2 = []
-m = True
 
-def recieve(m):
-    a = int(input('Type your number: '))
-    if a >= 0:
-        l.append(str(a))
-    return l
+'''
+use for collect the recieved number as string in the list
+number refers to amount of number we would like to collect from user (length of list)
+'''
+def recieve(number):
+    while number > 0:
+        a = int(input('Type your number: '))
+        if a >= 0:
+            l.append(str(a))
+            number -= 1
+    return l  
 
-while m == True:
-    recieve(m)
-    if len(l) == 4:
-        m = False
+#use for create the biggest number by concatenate string
+def biggestnum(k):
+    biggest = ""
+    for i in (k):
+        biggest += i
+    return biggest
 
-n = True
+#ask for number and stop when length of the list l = 4
+recieve(4)      
+
+'''
+compare number in string type to find the biggest and arrange in order
+by compare XY > YX or not then compare it again with b to make sure 
+that it is the biggest number and collect the number in l2 
+then remove it from l
+(at first, assign b as '0' because it is the possible smallest number)
+'''
 while len(l) > 0:
     b = '0'
     for i in range (len(l)):
@@ -36,9 +52,6 @@ while len(l) > 0:
         l2.append(l[0])
         l.remove(l[0])
 
-biggest = ""
-for i in range (len(l2)):
-    biggest += l2[i]
-
-print(biggest)
-
+#show the output
+print(biggestnum(l2))
+    
