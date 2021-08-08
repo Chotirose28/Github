598e2c37f48b36ec2ee22ef7d87052a6909cdb54
Farishine28 <chotirose.pra@gmail.com>
2021-08-08 20:55:36 +0700

    08082021 HW5

--- HW5.py
+++ HW5.py
@@ -9,13 +9,26 @@ Decision variable:  x1 = vanilla ice cream
                     x2 = strawberry ice cream
 Objective function: Maximum profit = 2x1 + 3x2
 Constraints: 0.5x1 + 0.2x2 <= 10
+            x1 + x2 <= 30
             x1 >= 0
             x2 >= 0
 '''
 import numpy as np
-A = np.array([2, 3]).reshape(1,2) #Coefficient for x1 and x2
-B = np.array([[0.5, 0.2]]) #Constraint left side
-C = np.array([10]) #Constraint right side
+C = np.array([[2, 3]]) #Coefficient for x1 and x2
+A = np.array([[0.5, 0.2],
+              [1, 1]]) #Constraint left side
+B = np.array([10,
+              30]) #Constraint right side
+
+#Constraint of x1 and x2 >= 0
+bnd = [(0, float("inf")),
+   (0, float("inf"))]
+
+from scipy.optimize import linprog
+opt = linprog(c=-C, A_ub=A, b_ub=B,bounds=bnd, method="revised simplex")
+print(opt)
+
+
 
 
 
